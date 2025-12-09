bbc41e80a5697b78f4adcaaa1e0b7579ff833521
Kevin Artya Christian <kevinartya245@gmail.com>
2025-12-09 00:13:56 +0000

    config codespace db

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Database Configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
-define('DB_PASS', 'Tumahaatuh12');
+define('DB_PASS', 'root');
 define('DB_NAME', 'tutorhub');
 
 // Create database connection
